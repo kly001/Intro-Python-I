e02f619673dbc144d00726712ec1e5d05432753e
kly001 <karenly1961@gmail.com>
2020-05-10 14:41:50 -0400

    args.py: need to finish last example

--- src/11_args.py
+++ src/11_args.py
@@ -15,19 +15,19 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
-'''
+
 def f2(*args):
     return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33'''
+print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -51,10 +51,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-def f4(**my_dict):
-    for key, value in my_dict.items():
-        print("key: %s, value: %s" 
-        (key, value))
+def f4(a,b):
+    print("key: a, value: " + str(a))
+    print("key: b, value: " + str(b))
 
 
 # Should print
@@ -75,3 +74,4 @@ d = {
 
 # How do you have to modify the f4 call below to make this work?
 f4(d)
+
