98db06ccbf7246eebb49dd21a59e24ae0de75186
kly001 <karenly1961@gmail.com>
2020-05-10 16:01:50 -0400

    file_io.py finished

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-with open("foo.txt", "r") as f:
-    print(f.read())
-    f.close()
+f = open("foo.txt", "r") 
+print(f.read())
+f.close()
 
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
@@ -21,8 +21,14 @@ with open("foo.txt", "r") as f:
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
-with open("bar.txt", "r") as f:
-    
+b = open("bar.text", "w")
+b.write("I am Sam.  ")
+b.write("Sam I Am.  ")
+b.write("Do you like green eggs and ham?  ")
+b.close()
 
-    
-    f.close()
+b= open("bar.text", "r")
+data = b.read()
+print(data)
+b.close()
+
