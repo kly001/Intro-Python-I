eea5ecc36c2cb238714235c7ab4d822068e608a2
kly001 <karenly1961@gmail.com>
2020-05-11 22:23:23 -0400

    added code/note from class review

--- src/11_args.py
+++ src/11_args.py
@@ -16,7 +16,7 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
-def f2(*args):
+def f2(*args): # * operator changes this to a tuple
     return sum(args)
 
 print(f2(1))                    # Should print 1
@@ -65,7 +65,7 @@ f4(a=12, b=30)
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+f4(city="Berkeley", population= 121240, founded="March 23, 1868")
 
 d = {
     "monster": "goblin",
